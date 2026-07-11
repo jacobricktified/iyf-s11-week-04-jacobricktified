d2f6c448c037a85474af0bb949a369b7583a799a
jacob gichira mbuco <mbucojacob@gmail.com>
2026-07-11 21:18:31 +0300

    Declare and initialize variables

---
+++ main.js
@@ -0,0 +1,33 @@
+// Variable declarations
+let name = "Your Name";
+let age = 25;
+const birthYear = 1999;
+
+// typeof operator
+console.log(typeof name);    // string
+console.log(typeof age);     // number
+console.log(typeof true);    // boolean
+
+// let vs const
+let score = 100;
+score = 150;  // Works!
+
+const PI = 3.14159;
+// PI = 3;  // Error! const cannot be reassigned
+
+
+// Exercise: Variable Practice
+
+// Declare variables
+let myName = "Jacob";
+let myAge = 30;
+let isStudent = true;
+let favoriteColors = ["Green", "Black", "Blue"];
+let today = new Date();
+
+// Display variables
+console.log("My name is:", myName);
+console.log("My age is:", myAge);
+console.log("Am I a student?", isStudent);
+console.log("My favorite colors are:", favoriteColors);
+console.log("Today's date is:", today);
