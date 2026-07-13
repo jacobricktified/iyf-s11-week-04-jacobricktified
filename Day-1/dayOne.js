3de6e12d505967302e410ba5e87de269600948f2
jacob gichira mbuco <mbucojacob@gmail.com>
2026-07-13 16:31:29 +0300

    Function to reverse a string using a loop

--- Day-1/dayOne.js
+++ Day-1/dayOne.js
@@ -1,4 +1,5 @@
 // Day 1: FizzBuzz
+//for loop: print fizzbuzz if divisible by 3 and 5, fizz if divisible by 3, buzz if divisible by 5, else print the number
 
 for (let i = 1; i <= 100; i++) {
 
---
+++ Day-2/dayTwo.js
@@ -0,0 +1,14 @@
+// using a loop
+// function to reverse a string using a loop
+function reverseString(str) {
+    let reversed = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+
+    return reversed;
+}
+
+console.log(reverseString("hello"));
+console.log(reverseString("JavaScript"));
